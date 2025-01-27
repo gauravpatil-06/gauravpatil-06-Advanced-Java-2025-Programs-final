14eb68958de47364ab709143392e4b584ac88b19
Gaurav Patil <gp949958@gmail.com>
2025-01-27 18:48:14 +0530

    Worked on data view practicals

---
+++ Advanced_Java_Practical_Programs/Practical 7/JTreeNodeDemo.java
@@ -0,0 +1,46 @@
+import javax.swing.*; 
+import javax.swing.tree.*; 
+import java.awt.*; 
+class JTreeNodeDemo extends JFrame 
+{ 
+	JTreeNodeDemo() 
+	{ 
+	   Container c=getContentPane(); 
+	   BorderLayout b1=new BorderLayout(); 
+	   c.setLayout(b1); 
+	   
+	   c.setBackground(Color.cyan);
+	   c.setForeground(Color.black);
+	   
+	   DefaultMutableTreeNode root=new DefaultMutableTreeNode("India"); 
+	   DefaultMutableTreeNode r1=new DefaultMutableTreeNode("Maharashtra"); 
+	   DefaultMutableTreeNode r2=new DefaultMutableTreeNode("Gujarat");
+	   root.add(r1); 
+	   root.add(r2); 
+		
+	   DefaultMutableTreeNode r11=new DefaultMutableTreeNode("Mumbai"); 
+	   DefaultMutableTreeNode r12=new DefaultMutableTreeNode("Pune"); 
+	   DefaultMutableTreeNode r13=new DefaultMutableTreeNode("Nashik"); 
+	   DefaultMutableTreeNode r14=new DefaultMutableTreeNode("Nagpur");  
+	 
+	   r1.add(r11); 
+	   r1.add(r12); 
+	   r1.add(r13); 
+	   r1.add(r14); 
+		
+	   JTree jt=new JTree(root); 
+		
+	   int v=ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED; 
+	   int h=ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED; 
+	   JScrollPane jsp=new JScrollPane(jt,v,h);
+	   c.add(jsp,BorderLayout.WEST);
+	} 
+	public static void main(String args[]) 
+	{ 
+		JTreeNodeDemo j1=new JTreeNodeDemo(); 
+		j1.setVisible(true); 
+		j1.setTitle("JTree Demo"); 
+		j1.setSize(600,600);     
+		j1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	} 
+}
---
+++ Advanced_Java_Practical_Programs/Practical 8/JTableDemo1.java
@@ -0,0 +1,38 @@
+import javax.swing.*; 
+import java.awt.*; 
+class JTableDemo1 extends JFrame 
+{ 
+	JTableDemo1() 
+	{ 
+	   Container c=getContentPane(); 
+	   
+	   BorderLayout b1=new BorderLayout(); 
+	   c.setLayout(b1); 
+	   
+	   String colHeads[]={"Name","Percentage","Grade"}; 
+	   String data[][]={{"Gaurav","99.90%","A++"}, 
+					   {"Vinod","98.00%","A+"},
+					   {"Yogita","98.50%","A+"},	 
+					   {"Akash","97.70%","A+"}, 
+					   {"Shivam","96.75%","A"},
+					   {"Vitthal","95.67%","A"}, 
+					   {"Srushti","86.03%","A"},
+					   {"Om","89.93%","C"}, 
+					   {"Rohan","92.75%","B"}};
+		
+	   JTable jt=new JTable(data,colHeads); 
+	   
+	   int v=ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED; 
+	   int h=ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED;   
+	   JScrollPane jsp=new JScrollPane(jt,v,h); 
+	   c.add(jsp,BorderLayout.CENTER); 
+	} 
+	public static void main(String args[]) 
+	{ 
+		 JTableDemo1 j1=new JTableDemo1(); 
+		 j1.setVisible(true); 
+		 j1.setTitle("JTable Demo"); 
+		 j1.setSize(600,600); 
+		 j1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	} 
+} 
